519401bb66418db8e4773f7af9373d7a66a5e4e0
Amanda-Katherine <walker.akw@gmail.com>
2020-10-20 02:05:25 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
-// Your code here
+function mapToNegativize(array) {
+    let negArray = []
+    
+    array.map(function(number) {
+        negArray.push(number * (-1))
+    })
+
+    return negArray
+}
+
+function mapToNoChange(array) {
+    return array
+}
+
+function mapToDouble(array) {
+    let doubledArray = []
+
+    array.map(function(number) {
+        doubledArray.push(number * 2)
+    })
+
+    return doubledArray
+}
+
+function mapToSquare(array) {
+    let squaredArray = []
+
+    array.map(function(number) {
+        squaredArray.push(number ** 2) 
+    })
+
+    return squaredArray
+}
+
+function reduceToTotal(array, startingPoint) {
+
+}
+
+function reduceToAllTrue(array) {
+
+}
+
+function reduceToAnyTrue(array) {
+
+}
+
